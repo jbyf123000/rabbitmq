9a11e462c8853e9270d7b771d6dff5de2c8c873a
jbyf <506921864@qq.com>
2021-06-15 15:50:55 +0800

    开启队列持久化和消息持久化

--- rabbitmq_1/src/main/java/com/jbyf/Consumer.java
+++ rabbitmq_1/src/main/java/com/jbyf/Consumer.java
@@ -17,13 +17,13 @@ public class Consumer {
         //创建通道
         Channel channel = connection.createChannel();
         //生产者和消费者的通道配置必须相同
-        channel.queueDeclare("hello",false,false,false,null);
+        channel.queueDeclare("hello2",true,false,false,null);
 
         //消费消息
         //参数1 : 指定消费队列名称
         //参数2 : 开启消费自动确认
         //参数3 : 消费消息时的回调接口
-        channel.basicConsume("hello",true,new DefaultConsumer(channel){
+        channel.basicConsume("hello2",true,new DefaultConsumer(channel){
             //最后一个参数: 消息队列中取出的消息
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
--- rabbitmq_1/src/main/java/com/jbyf/Provider.java
+++ rabbitmq_1/src/main/java/com/jbyf/Provider.java
@@ -4,6 +4,7 @@ import com.jbyf.utils.RabbitMQUtils;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -22,18 +23,18 @@ public class Provider{
 
         //通道绑定对应的消息队列
         //参数1 : queue       队列名称    如果队列不存在则自动创建
-        //参数2 : durable     定义队列特性是否要持久化
+        //参数2 : durable     定义队列特性是否要持久化    如果没有设置持久化,rabbit服务重启时会销毁队列,队列中的消息也会丢失,如果要保证消息的持久化,需要在发布消息时添加额外的设置
         //参数3 : exclusive   是否独占队列--当前队列是否只为本链接使用
         //参数4 : autoDelete  是否在消费完成后自动删除队列
         //参数5 : 额外参数
-        channel.queueDeclare("hello",false,false,false,null);
+        channel.queueDeclare("hello2",true,false,false,null);
 
         //发布消息
         //参数1 : exchange    交换机名称
         //参数2 : 队列名称
-        //参数3 : 传递消息的额外设置
+        //参数3 : 传递消息的额外设置   可以在此处设置队列消息持久化  MessageProperties.PERSISTENT_TEXT_PLAIN:持久化消息
         //参数4 : 消息的具体内容
-        channel.basicPublish("", "hello",null,"hello,rabbitmq".getBytes());
+        channel.basicPublish("", "hello2", MessageProperties.PERSISTENT_TEXT_PLAIN,"hello,rabbitmq".getBytes());
 
         //关闭连接
         RabbitMQUtils.closeConnectionAndChanel(channel,connection);
