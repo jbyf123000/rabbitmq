48df1fd043dab6aa4d942ce429eb0d40b0fb61c6
jbyf <506921864@qq.com>
2021-06-15 21:39:05 +0800

    work queue 的缺点: 如果有一个消费者较慢,会拖累效率

--- rabbitmq_2/src/main/java/workqueue/Customer1.java
+++ rabbitmq_2/src/main/java/workqueue/Customer1.java
@@ -14,7 +14,7 @@ public class Customer1 {
             channel.basicConsume("work",true,new DefaultConsumer(channel){
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                    System.out.println("消费者1"+new String(body));
+                    System.out.println("消费者1:"+new String(body));
                 }
             });
         }catch (Exception e){
--- rabbitmq_2/src/main/java/workqueue/Provider.java
+++ rabbitmq_2/src/main/java/workqueue/Provider.java
@@ -15,8 +15,8 @@ public class Provider {
             //声明队列
             channel.queueDeclare("work",true,false,false,null);
             //发布消息
-            for (int i = 0; i < 10; i++) {
-                channel.basicPublish("","work",null,(i+"hello work queue").getBytes());
+            for (int i = 1; i <= 20; i++) {
+                channel.basicPublish("","work",null,(i+"--hello work queue").getBytes());
             }
             RabbitMQUtils.closeConnectionAndChanel(channel,connection);
         }catch (Exception e){
